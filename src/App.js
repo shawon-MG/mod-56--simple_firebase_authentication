5c0feaaf5a84ef100b73a88e38ab3c7cf53f6580
shawon-MG <work.shawomg@gmail.com>
2022-04-13 01:28:17 +0600

    auth provider, signInWithPopup added & imported from firebase. Event handler added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import './App.css';
 import app from "./firebase.init";
 
 const auth = getAuth(app);
 
 function App() {
+
+  const provider = new GoogleAuthProvider();/* etar jonno pop-up asbe */
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider) /*firebase theke imported etao*/
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch(error => {
+        console.log('error : ', error);
+      })
+  }
   return (
     <div className="App">
+
+      <button onClick={handleGoogleSignIn} >Google Sign In</button>
 
     </div>
   );
