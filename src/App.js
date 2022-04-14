1d558c1ef8e656a5b1e77a43cbe2a6780f8e2baf
shawon-MG <work.shawomg@gmail.com>
2022-04-14 22:57:44 +0600

    signOut is imported from firebase, sign out btn added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { useState } from 'react';
 import './App.css';
 import app from "./firebase.init";
@@ -22,11 +22,26 @@ function App() {
         console.log('error : ', error);
       })
   }
+
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+        console.log("Out")
+      })
+      .catch((error) => {
+        setUser({});
+        console.log("error")
+      })
+  }
   return (
     <div className="App">
 
       <button onClick={handleGoogleSignIn} >Google Sign In</button>
+      <button onClick={handleGoogleSignOut}>Google Sign Out</button>
       <h2>Name: {user.displayName}</h2>
+      <h4>Email: {user.email}</h4>
+      <h4>UniqueID: {user.uid}</h4>
 
     </div>
   );
