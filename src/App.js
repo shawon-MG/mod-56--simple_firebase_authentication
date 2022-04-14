ff829e4cdfad66de9f30fbed866befeb14aa0ca2
shawon-MG <work.shawomg@gmail.com>
2022-04-14 23:33:24 +0600

    Github sign in is added)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { useState } from 'react';
 import './App.css';
 import app from "./firebase.init";
@@ -9,25 +9,36 @@ function App() {
 
   const [user, setUser] = useState({});
 
-  const provider = new GoogleAuthProvider();/* etar jonno pop-up asbe */
+  const googleProvider = new GoogleAuthProvider();/* etar jonno pop-up asbe */
+  const githubProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider) /*firebase theke imported etao*/
+    signInWithPopup(auth, googleProvider) /*firebase theke imported etao*/
       .then(result => {
         const user = result.user;
         setUser(user);
         console.log(user);
       })
       .catch(error => {
-        console.log('error : ', error);
+        console.error('error : ', error);
       })
-  }
+  };
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  };
 
   const handleGoogleSignOut = () => {
     signOut(auth)
       .then(() => {
         setUser({});
-        console.log("Out")
       })
       .catch((error) => {
         setUser({});
@@ -38,7 +49,8 @@ function App() {
     <div className="App">
 
       <button onClick={handleGoogleSignIn} >Google Sign In</button>
-      <button onClick={handleGoogleSignOut}>Google Sign Out</button>
+      <button onClick={handleGoogleSignOut}>Google Sign Out</button> <br />
+      <button onClick={handleGithubSignIn}>GitHub Sign In</button>
       <h2>Name: {user.displayName}</h2>
       <h4>Email: {user.email}</h4>
       <h4>UniqueID: {user.uid}</h4>
